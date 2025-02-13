6dd8391fbe920cef19b3c8cf08a067b5ba1c1e52
mounika-gurujukota <mounika.gurujukota@heits.in>
2025-02-13 10:23:58 +0530

    Sprint boot backend base project

---
+++ src/test/java/com/Datientis/Data_Migration/DataMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Datientis.Data_Migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
